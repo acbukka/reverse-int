9ce4acefd2aa1682f11f7663e69339081b5f36b2
acbukka <thesweylis@gmail.com>
2022-01-05 17:15:48 +0700

    feat: reverseInt func add

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(num) {
+  let x = num.toString().length;
+  let revInt = "";
+  while (x > 0) {
+    revInt = revInt + num.toString()[x - 1];
+    x--;
+  }
+  return parseFloat(revInt);
+};
