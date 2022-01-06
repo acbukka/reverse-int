96755102fde7ae518108999fd0f1fe886fe5feda
acbukka <thesweylis@gmail.com>
2022-01-06 10:56:45 +0700

    add func shorter

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-module.exports = function reverse(num) {
-  let x = num.toString().length;
-  let revInt = "";
-  while (x > 0) {
-    revInt = revInt + num.toString()[x - 1];
-    x--;
-  }
-  return parseFloat(revInt);
-};
+module.exports = function reverse(num) { return parseFloat(num.toString().split('').reverse().toString().replace(/,/g, '')); }
+
+// первый способ:
+// module.exports = function reverse(num) {
+//   let x = num.toString().length;
+//   let revInt = "";
+//   while (x > 0) {
+//     revInt = revInt + num.toString()[x - 1];
+//     x--;
+//   }
+//   return parseFloat(revInt);
+// };
